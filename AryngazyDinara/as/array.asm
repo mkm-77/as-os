8236250611befca09842254db5758c888db99e77
mkm-77 <62931438+mkm-77@users.noreply.github.com>
2020-04-04 21:55:20 +0600

    Add files via upload

---
+++ AryngazyDinara/as/array.asm
@@ -0,0 +1,39 @@
+;ml64 /c array.asm
+;link /subsystem:console array.obj kernel32.Lib MyLib-64.obj /ENTRY:main /largeaddressaware:no
+
+; Scanning an array (array.asm)
+; Scan an array for the first nonzero value.
+
+ExitProcess 	proto
+WriteInt64		proto
+WriteString     proto
+Crlf 			proto
+
+.data
+intArray SQWORD 0,0,0,0,1,20,35,-12,66,4,0
+;intArray SQWORD 1,0,0,0 ; alternate test data
+;intArray SQWORD 0,0,0,0 ; alternate test data
+;intArray SQWORD 0,0,0,1 ; alternate test data
+noneMsg BYTE "A non-zero value was not found",0
+
+.code
+main PROC
+	mov rbx,OFFSET intArray					 ; point to the array
+	mov rcx,LENGTHOF intArray 				 ; loop counter
+L1: 
+	cmp WORD PTR [rbx],0 					 ; compare value to zero
+	jnz found 								 ; found a value
+	add rbx, TYPE rbx 								 ; point to next
+	loop L1 								 ; continue the loop
+	jmp notFound 							 ; none found
+found: 										 ; display the value
+	movsx rax,WORD PTR[rbx]     	 		 ; sign-extend into RAX
+	call WriteInt64
+	jmp quit
+notFound: 									 ; display "not found" message
+	mov rdx,OFFSET noneMsg
+	call WriteString
+quit:
+	call Crlf
+main ENDP
+END
