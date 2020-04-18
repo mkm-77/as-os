a80dc86a3989143ed14f1f70682ff2eb0da457fb
mkm-77 <62931438+mkm-77@users.noreply.github.com>
2020-04-18 10:54:42 +0300

    Update and rename new.asm to messagebox.asm

---
+++ AsemAsylbek/as/messagebox.asm
@@ -0,0 +1,25 @@
+;ml64 /c messagebox.asm
+;link /subsystem:console messagebox.obj kernel32.Lib /ENTRY:main /largeaddressaware:no
+
+;ml64 new.asm /link /subsystem:console messagebox.obj /ENTRY:main /largeaddressaware:no
+
+; Sample x64 Assembly Program
+; Chris Lomont 2009 www.lomont.org
+
+include include\invoke_macros.asm
+includelib lib\kernel32.lib
+includelib lib\user32.lib
+includelib lib\Gdi32.lib
+
+extrn ExitProcess: PROC       ; external functions in system libraries
+extrn MessageBoxA: PROC
+.data
+caption db '64-bit hello!', 0
+message db 'Hello World!', 0
+.code
+main PROC
+  nvk MessageBoxA, 0, ADDR message, ADDR caption, 0
+  mov    rcx, rax             ; uExitCode = MessageBox(...)
+  call ExitProcess
+main ENDP
+End
