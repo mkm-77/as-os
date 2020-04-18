3fdaaa68ed8415c74e22fabd052254ff3d1462b3
mkm-77 <62931438+mkm-77@users.noreply.github.com>
2020-04-18 13:46:21 +0600

    Update message.asm

--- TomirisOrazakhimova/as/message.asm
+++ TomirisOrazakhimova/as/message.asm
@@ -1,12 +1,11 @@
-;ml64 /c message.asm /link /subsystem:console message.obj MyLib-64.obj /ENTRY:main /largeaddressaware:no
+;ml64  message.asm /link /subsystem:console message.obj /ENTRY:main /largeaddressaware:no
 
 ; Sample x64 Assembly Program
 ; Chris Lomont 2009 www.lomont.org
 
-include include\invoke_macros.asm
+include include\invoke_macros.asm	
 includelib lib\kernel32.lib
 includelib lib\user32.lib
-includelib lib\Gdi32.lib
 
 extrn ExitProcess: PROC   ; external functions in system libraries
 extrn MessageBoxA: PROC
@@ -14,9 +13,9 @@ extrn MessageBoxA: PROC
 caption db '64-bit hello!', 0
 message db 'Hello World!', 0
 .code
-Start PROC
-  nvk MessageBoxA, 0, message, caption, 0
+main PROC
+  nvk MessageBoxA, 0,ADDR message,ADDR caption, 0
   mov    ecx, eax     ; uExitCode = MessageBox(...)
   call ExitProcess
-Start ENDP
+main ENDP
 End
