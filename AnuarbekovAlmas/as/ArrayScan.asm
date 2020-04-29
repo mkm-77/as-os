e3a1fc06c1fc29b44119a7c1933b3b500c21e798
mkm-77 <62931438+mkm-77@users.noreply.github.com>
2020-04-29 16:27:40 +0600

    Add files via upload

--- AnuarbekovAlmas/as/ArrayScan.asm
+++ AnuarbekovAlmas/as/ArrayScan.asm
@@ -1,50 +1,39 @@
-; Scanning an Array             (ArrayScan.asm)
-; Scan an array for the first nonzero value.
-
-;ml64 /c MyLib-64.asm
 ;ml64 /c ArrayScan.asm
 ;link /subsystem:console ArrayScan.obj kernel32.Lib MyLib-64.obj /ENTRY:main /largeaddressaware:no
 
-ExitProcess PROTO
-WriteInt64 PROTO
-WriteString PROTO
-Crlf PROTO
+; Scanning an Array (ArrayScan.asm)
+; Scan an array for the first nonzero value.
 
+ExitProcess 	proto
+WriteInt64		proto
+WriteString     proto
+Crlf 			proto
 
- 
- .data 
- intArray  QWORD  0,0,0,20,35,-12,66,4,0 
- 
-;intArray QWORD  1,0,0,0 ; alternate test data 
-;intArray QWORD  0,0,0,0
-;intArray QWORD  0,0,0,1 
- noneMsg  BYTE "A non-zero value was not found",0
- 
- .code 
- main PROC
-	mov rbx,OFFSET intArray						; point to the array
-	mov rcx,LENGTHOF intArray 					; loop counter
-	 		
-L1:
-	cmp   WORD PTR [rbx],0 						; compare value to zero
-	jnz   found 								; found a value
-	add   rbx,TYPE rbx							; point to next 
-	loop  L1 									; continue the loop 
-	jmp   notFound 								; none found 
-found: 											; display the value 
-	movsx rax,WORD PTR[rbx] 					; sign-extend into EAX 
-	call  WriteInt64 
-	jmp   quit
-notFound: 										; display "not found" message
-	mov   rdx,OFFSET noneMsg
-	call WriteString 
+.data
+intArray SQWORD 0,0,0,0,1,20,35,-12,66,4,0
+;intArray SQWORD 1,0,0,0 ; alternate test data
+;intArray SQWORD 0,0,0,0 ; alternate test data
+;intArray SQWORD 0,0,0,1 ; alternate test data
+noneMsg BYTE "A non-zero value was not found",0
+
+.code
+main PROC
+	mov rbx,OFFSET intArray					 ; point to the array
+	mov rcx,LENGTHOF intArray 				 ; loop counter
+L1: 
+	cmp WORD PTR [rbx],0 					 ; compare value to zero
+	jnz found 								 ; found a value
+	add rbx, TYPE rbx 								 ; point to next
+	loop L1 								 ; continue the loop
+	jmp notFound 							 ; none found
+found: 										 ; display the value
+	movsx rax,WORD PTR[rbx]     	 		 ; sign-extend into RAX
+	call WriteInt64
+	jmp quit
+notFound: 									 ; display "not found" message
+	mov rdx,OFFSET noneMsg
+	call WriteString
 quit:
-	call Crlf 
-	
-	
-call  ExitProcess
-main endp
-PUBLIC main
-end
-
- 
+	call Crlf
+main ENDP
+END
