4f2b1b6859a47b5b061ca182f3117024a9f2d757
mkm-77 <62931438+mkm-77@users.noreply.github.com>
2020-04-29 16:29:27 +0600

    Add files via upload

---
+++ AnuarbekovAlmas/os/gitdir1.sh
@@ -0,0 +1,1 @@
+wget --no-check-certificate https://github.com/mkm-77/as-os -O- | grep 'href="/mkm-77/as-os/tree/master/' | sed s/'<.*open "'// | sed s/'title=".*id'// | sed s/'=.* '// | sed s/'>.*>'//
