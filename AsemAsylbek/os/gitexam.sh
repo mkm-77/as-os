e6272b37d07ee026bd67a0c20a8e089eb2da3f34
mkm-77 <62931438+mkm-77@users.noreply.github.com>
2020-05-14 11:31:33 +0600

    Update and rename gitdir1.sh to gitexam.sh

---
+++ AsemAsylbek/os/gitexam.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget --no-check-certificate https://github.com/mkm-77/as-os -O- | grep 'href="/mkm-77/as-os/tree/master/' | sed s/'<.*open "'// | sed s/'title=".*id'// | sed s/'=.* '// | sed s/'>.*>'// > dir.txt
